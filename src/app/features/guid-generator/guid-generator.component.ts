0cf08abe16176f77786c68811de96082e278b0d9
Serhii Kimlyk <serhii.kimlyk@wolterskluwer.com>
2025-11-13 13:08:51 +0200

    chore: add basic features

--- src/app/features/guid-generator/guid-generator.component.ts
+++ src/app/features/guid-generator/guid-generator.component.ts
@@ -1,9 +1,34 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject, signal } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { catchError } from 'rxjs';
+import { API_URL } from 'src/app/app.config';
 
 @Component({
   selector: 'app-guid-generator',
   standalone: true,
   templateUrl: 'guid-generator.component.html',
   styleUrl: 'guid-generator.component.scss',
+  imports: [MatButtonModule],
 })
-export class GuidGeneratorComponent {}
+export class GuidGeneratorComponent {
+  private readonly httpClient = inject(HttpClient);
+  private readonly apiBase = inject(API_URL);
+
+  protected guids = signal<string[]>([]);
+
+  onGenerate(): void {
+    const guids = this.guids();
+    const guid$ = this.httpClient
+      .get<string>(`${this.apiBase}/api/guid`)
+      .pipe(catchError(() => 'Error'))
+      .subscribe((data) => {
+        guids.push(data);
+        this.guids.set(guids);
+      });
+  }
+
+  onClear(): void {
+    this.guids.set([]);
+  }
+}
--- src/app/features/string-encode/string-encode.component.ts
+++ src/app/features/string-encode/string-encode.component.ts
@@ -1,9 +1,62 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatAnchor, MatButtonModule } from '@angular/material/button';
+import { API_URL } from 'src/app/app.config';
+import { HttpClient } from '@angular/common/http';
+import { MatIconModule } from '@angular/material/icon';
+import { Clipboard } from '@angular/cdk/clipboard';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-string-encoder',
   standalone: true,
   templateUrl: 'string-encode.component.html',
   styleUrl: 'string-encode.component.scss',
+  imports: [
+    FormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatAnchor,
+    MatButtonModule,
+    MatIconModule,
+  ],
 })
-export class StringEncoderComponent {}
+export class StringEncoderComponent {
+  protected readonly text: string | undefined = undefined;
+  protected readonly result = signal<string | undefined>(undefined);
+
+  private apiBase = inject(API_URL);
+  private http = inject(HttpClient);
+  private clipboard = inject(Clipboard);
+  private snackBar = inject(MatSnackBar);
+
+  onEncode(): void {
+    this.http
+      .post<{ converted: string }>(`${this.apiBase}/api/base64/to`, {
+        inputText: this.text,
+      })
+      .subscribe(({ converted }) => this.result.set(converted));
+  }
+
+  onDecode(): void {
+    this.http
+      .post<{ converted: string }>(`${this.apiBase}/api/base64/from`, {
+        inputText: this.text,
+      })
+      .subscribe(({ converted }) => this.result.set(converted));
+  }
+
+  onCopy(): void {
+    const text = this.result();
+    if (text) {
+      this.clipboard.copy(text);
+      this.snackBar.open('Copied to clipboard!', 'Close', {
+        duration: 2000,
+        horizontalPosition: 'center',
+        verticalPosition: 'bottom',
+      });
+    }
+  }
+}
